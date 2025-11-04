8cb536d090603d0ffc1d5a3c36c689519d18bc5d
gfhjdf <nurullayevjahongir0108@gmail.com>
2025-11-05 00:01:58 +0500

    Create hw.py

---
+++ lesson-1/homework/hw.py
@@ -0,0 +1,21 @@
+### 1
+side = int(input())
+
+print("The perimeter of the given square", side*4)
+print("The area of the given square", side**2)
+### 2
+d = int(input())
+
+print("Diameter of the circle is", d*3.14)
+### 3
+a = int(input())
+b = int(input())
+
+print("The mean of two given numbers: a and b is", int((a + b) / 2))
+### 4
+a = int(input())
+b = int(input())
+
+print("The sum of the two given numbers: a and b is", int(a + b))
+print("The product of the two given numbers: a and b is", int(a * b))
+print("The square of the two given numbers: a and b are", int(a*a), "and", int(b*b))
